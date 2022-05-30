eb85efe906aed6b52455495f58851d746b78351a
GuardiaN123321 <53257681+GuardiaN123321@users.noreply.github.com>
2022-05-30 13:37:59 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,29 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.751574, 37.573856],
+            zoom: 9
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: '/dollar.png',
+            // Размеры метки.
+            iconImageSize: [30, 30]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark);
+});
